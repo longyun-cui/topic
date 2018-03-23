a2ea1a159761e85b0c0323ee95ebb0198093fd5b
longyun <longyun@longyundeMacBook-Pro.local>
2018-03-23 18:58:31 +0800

    style

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -151,14 +151,18 @@ desired effect
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
 
-                    <li class="visible-sm" style="display:none;">
-                        @if(!Auth::check())
-                            <a href="{{url('/login')}}"><i class="fa fa-circle-o"></i> <span>登录</span></a>
-                            <a href="{{url('/register')}}"><i class="fa fa-circle-o"></i> <span>注册</span></a>
-                        @else
-                            <a href="{{url('/home')}}"><i class="fa fa-home text-default"></i> <span>返回后台</span></a>
-                        @endif
-                    </li>
+                    @if(!Auth::check())
+                        <li class="visible-sm" style="display:none;">
+                            <a href="{{url('/login')}}"><i class="fa fa-circle-o"></i> <span> 登录</span></a>
+                        </li>
+                        <li class="visible-sm" style="display:none;">
+                            <a href="{{url('/register')}}"><i class="fa fa-circle-o"></i> <span> 注册</span></a>
+                        </li>
+                    @else
+                        <li class="visible-sm" style="display:none;">
+                            <a href="{{url('/home')}}"><i class="fa fa-home text-default"></i> <span> 返回后台</span></a>
+                        </li>
+                    @endif
 
                     <li class="dropdown notifications-menu visible-xs">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
